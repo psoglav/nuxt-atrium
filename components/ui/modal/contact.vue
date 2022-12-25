f67c01734c406a3e1119758f337b75f5ec5c0edd
Andrey Nevinny <psoglav.ih8u@gmail.com>
2022-12-25 13:58:16 +0300

    repair contact modal

--- components/ui/modal/contact.vue
+++ components/ui/modal/contact.vue
@@ -1,26 +1,21 @@
 <script setup>
-import { computed } from 'vue'
-import { useApp } from '@/stores/app'
-
-import Modal from './Modal.vue'
-import Button from '@/components/button/Button.vue'
-import Input from '@/components/input/Input.vue'
+import { useApp } from '@/store'
 
 const app = useApp()
 const showContactUs = computed(() => app.contactUsModal)
 </script>
 
 <template>
-  <Modal :show="showContactUs" @close="app.toggleContactUsModal">
+  <ui-modal :show="showContactUs" @close="app.toggleContactUsModal">
     <template #title>{{ $t('contact.call') }}</template>
     <template #content>
       <div class="flex flex-col gap-4 w-[20rem]">
-        <Input :label="$t('form.name')" placeholder="Type your name" />
-        <Input :label="$t('form.phone')" placeholder="Type your phone number" />
+        <ui-input :label="$t('form.name')" placeholder="Type your name" />
+        <ui-input :label="$t('form.phone')" placeholder="Type your phone number" />
       </div>
     </template>
     <template #actions>
-      <Button no-blur uppercase primary>{{ $t('form.send') }}</Button>
+      <ui-button no-blur uppercase primary>{{ $t('form.send') }}</ui-button>
     </template>
-  </Modal>
+  </ui-modal>
 </template>
